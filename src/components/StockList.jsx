de5d8239e8fbb8884468fddf1e9af890e711c58d
Tiago Morgado <Tiago.alcm@icloud.com>
2023-05-16 23:20:36 +0200

    Extract data and symbol from API call

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -18,9 +18,15 @@ export const StockList = () => {
                     })
                 }))
 
-                console.log(responses)
+                const data = responses.map((response) => {
+                    return{
+                        data: response.data,
+                        symbol: response.config.params.symbol
+                    }   
+                })
+                console.log(data)
                 if(isMounted) {
-                    setStock(responses)
+                    setStock(data)
                 }
             } catch(e) {
                 
